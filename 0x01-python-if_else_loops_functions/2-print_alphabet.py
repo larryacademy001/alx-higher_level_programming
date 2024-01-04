841bfabaff102063b8b60e26997b326fd91ef70d
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-01-04 04:04:29 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ascii_alphabet in range(97, 123, 1):
-    print("{}".format(chr(ascii_alphabet)), end="")
+for alphabet in range(97, 123, 1):
+    print("{}".format(chr(alphabet)), end="")
