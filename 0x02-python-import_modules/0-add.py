a2509f32ee806c947b801e24f28cbef260d954e4
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-01-05 05:57:16 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # 0_add.py
-# program that imports the function def add(a, b): 
-# from the file add_0.py and prints the result of the 
-# addition 1 + 2 = 3
 
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
