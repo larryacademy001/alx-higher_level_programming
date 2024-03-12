914aa70bb4ff52727e62184f4dad2cdf9858dcb5
larryacademy001 <larryacademy@gmail.com>
2024-03-13 00:42:31 +0100

    Added comments

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,6 @@
+-- displays teh average temp by city
+-- ordered by temperature desc
+
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,5 @@
+-- create a table second_table in hbtn_0c_0
+-- add multiple rows
 CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256),
 score INT);
