e35c65695228f7a231997299d46861cb127cc886
larryacademy001 <larryacademy@gmail.com>
2024-03-13 09:04:40 +0100

    all in

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,10 @@
 -- Lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- Records are ordered by descending rating.
-SELECT `title`, SUM(`rate`) AS `rating`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_ratings` AS r
-       ON t.`id` = r.`show_id`
- GROUP BY `title`
- ORDER BY `rating` DESC;
+
+SELECT ts.`title`, SUM(tsr.`rate`) AS `rating`
+FROM `tv_shows` AS ts
+INNER JOIN `tv_show_ratings` AS tsr
+ON ts.`id` = tsr.`show_id`
+GROUP BY ts.`title`
+ORDER BY `rating` DESC;
  
