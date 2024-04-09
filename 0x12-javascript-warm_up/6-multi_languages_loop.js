a8f24e597d6477be26767a68c8ac76676586e7bc
larryacademy001 <larryacademy@gmail.com>
2024-04-09 03:48:22 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const stringValue = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < stringValue.length; i++) {
-  console.log(stringValue[i]);
+const string = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < string.length; i++) {
+  console.log(string[i]);
 }
