68415a641734c8c4ff007408a11572669087b072
larryacademy001 <larryacademy@gmail.com>
2024-04-09 03:52:40 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const ss = parseInt(process.argv[2]);
 if (isNaN(ss)) {
   console.log('Missing size');
 } else {
-    for (let i = 0; i < ss; i++) {
-      console.log('X'.repeat(ss));
-    }
+  for (let i = 0; i < ss; i++) {
+    console.log('X'.repeat(ss));
+  }
 }
