8e839fac54b2e3461605ce9af7a8d2e3d11cee31
larryacademy001 <larryacademy@gmail.com>
2024-04-09 03:39:41 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fa = process.argv[2];
 if (fa === undefined) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log(fa);
+  console.log(fa);
 }
