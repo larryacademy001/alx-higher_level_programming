262fbab47bc3695ea8cbce669c0c79d9dfa592c8
larryacademy001 <larryacademy@gmail.com>
2024-04-09 03:53:44 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const fa = parseInt(process.argv[2]);
 const sa = parseInt(process.argv[3]);
 function add (a, b) {
   if (isNaN(a) || isNaN(b)) {
-      return NaN;
+    return NaN;
   }
   return a + b;
 }
